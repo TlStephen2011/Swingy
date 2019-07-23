1f7c12625d87d7868ccf83b634448c3bb78e18c6
Tyrone Stephen <tstephen@student.wethinkcode.co.za>
2019-07-23 14:54:02 +0200

    have basic interface for GUI

--- swingy/src/main/java/za/co/wethinkcode/swingy/App.java
+++ swingy/src/main/java/za/co/wethinkcode/swingy/App.java
@@ -1,12 +1,16 @@
 package za.co.wethinkcode.swingy;
 
 import za.co.wethinkcode.controllers.GameController;
+import za.co.wethinkcode.views.GUI;
+import za.co.wethinkcode.views.Viewable;
 
 public class App 
 {
     public static void main( String[] args )
     {
-    	GameController controller = new GameController();
-    	while (controller.run()) {}
+    	//GameController controller = new GameController();
+    	//while (controller.run()) {}
+    	
+    	Viewable v = new GUI();
     }
 }
--- swingy/src/main/java/za/co/wethinkcode/views/GUI.java
+++ swingy/src/main/java/za/co/wethinkcode/views/GUI.java
@@ -4,18 +4,79 @@ import za.co.wethinkcode.models.Artifact;
 import za.co.wethinkcode.models.GameBoard;
 import za.co.wethinkcode.models.Hero;
 import za.co.wethinkcode.models.Villain;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class GUI implements Viewable {
 
+	private JFrame mainFrame; 
+	private JButton loadGame;
+	private JButton newGame;
+	private JMenuBar menu;
+	private JPanel mainPanel;
+	
+	public GUI() {
+		mainFrame = new JFrame("Swingy");
+		mainFrame.setSize(1200, 800);
+		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		mainFrame.setResizable(false);				
+		
+		menu = new JMenuBar();
+		JMenu m1 = new JMenu("OPTIONS");
+		JMenu m2 = new JMenu("HELP");
+		menu.add(m1);
+		menu.add(m2);
+		JMenuItem m11 = new JMenuItem("Switch to Console");
+		JMenuItem m12 = new JMenuItem("Quit Game");
+		JMenuItem m21 = new JMenuItem("About Swingy");
+		m1.add(m11);
+		m1.add(m12);
+		m2.add(m21);
+
+		loadGame = new JButton("Load Game");
+		newGame = new JButton("New Game");
+		loadGame.addActionListener(new ActionListener() {			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				//TODO implement load game
+			}
+		});
+		
+		loadGame.setBounds(400, 600, 100, 50);
+		newGame.setBounds(700, 600, 100, 50);
+		
+		JLabel title = new JLabel("Welcome to Swingy");
+		title.setFont(new Font("Monaco", Font.PLAIN, 20));
+		title.setBounds(500, 300, 400, 100);
+		
+		mainPanel = new JPanel();
+		mainPanel.setLayout(null);
+		
+		mainPanel.add(loadGame);
+		mainPanel.add(newGame);
+		mainPanel.add(title);
+		
+		mainFrame.getContentPane().add(menu, BorderLayout.SOUTH);
+		mainFrame.getContentPane().add(mainPanel);
+		mainFrame.setVisible(true);
+//		loadGame = new JButton("Load Game");
+//		newGame = new JButton("New Game");
+//		mainFrame.getContentPane().add(loadGame);
+//		mainFrame.getContentPane().add(newGame);
+	}
+	
 	@Override
 	public void display(GameBoard g, String x) {
 		// TODO Auto-generated method stub
-
+		
 	}
 
 	@Override
 	public Hero newHero() {
 		// TODO Auto-generated method stub
+		System.out.println("Displayed load game");
 		return null;
 	}
 
